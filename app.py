ca54f29e6d621ce248a00b89f8fcb2083a754134
Pranav Mujumdar <psmujumdar@gmail.com>
2019-02-18 13:46:43 +0530

    Linking with GuI
    Ongoing, Need to refine the code, and take directory path from GUI

--- app.py
+++ app.py
@@ -1,89 +1,150 @@
 import os
 import shutil
+import tkinter as tk
+import glob
+from pathlib import Path
 
-import os
-import shutil
-import tkinter
+
+window = tk.Tk()
+window.title('Organiser')
+window.geometry("480x360")
+
+#Label - Location
+label1=tk.Label(text="please enter the location: ")
+label1.pack()
+#grid(column=0, row=1)		---- cannot use grid() with pack()
+
+#Entry - Location
+entry1=tk.Entry()
+entry1.pack()
+entry1=tk.StringVar()		#---- maybe we cannot use this with entry() variable
+#grid(column=2, row=1)		---- cannot use grid() with pack()
+
+
+#Function Submit
+def Submit():
+	Sorter(all=i1.get(),Docs=i2.get(),Images=i3.get(),Videos=i4.get(),WebPages=i9.get(),ArchiveFiles=i5.get(),AudioFiles=i6.get(),Setups=i7.get(), ShellScripts=i8.get(), XML=i10.get())
+
+#for i in range(0 to 7)		---- tried using for loop, unable, try again
+i1=tk.IntVar()
+i2=tk.IntVar()
+i3=tk.IntVar()
+i4=tk.IntVar()
+i5=tk.IntVar()
+i6=tk.IntVar()
+i7=tk.IntVar()
+i8=tk.IntVar()
+i9=tk.IntVar()
+i10=tk.IntVar()
+# Actual Organiser
+
+DIRECTORIES = {
+"WebPages":["html5", "html", "htm", "xhtml"],
+"Images":["jpeg", "jpg", "tiff", "gif", "bmp", "png", "bpg", "svg","heif", "psd"],
+"Videos":["avi", "flv", "wmv", "mov", "mp4", "webm", "vob", "mng", "qt", "mpg", "mpeg", "3gp", "mkv"],
+"Documents":["txt", "in", "out","pdf","oxps", "epub", "pages", "docx", "doc", "fdf", "ods","odt", "pwi", "xsn", "xps", "dotx", "docm", "dox","rvg", "rtf", "rtfd", "wpd", "xls", "xlsx", "ppt","pptx"],
+"ArchiveFiles":["a", "ar", "cpio", "iso", "tar", "gz", "rz", "7z","dmg", "rar", "xar", "zip"],
+"AudioFiles":["aac", "aa", "aac", "dvf", "m4a", "m4b", "m4p", "mp3",".msv", "ogg", "oga", "raw", "vox", "wav", "wma"],
+"XML":["xml"],
+"Setups":["exe"],
+"ShellScripts":["sh"],
+}
+
+WebPages=["html5", "html", "htm", "xhtml"]
+Images=["jpeg", "jpg", "tiff", "gif", "bmp", "png", "bpg", "svg","heif", "psd"]
+Videos=["avi", "flv", "wmv", "mov", "mp4", "webm", "vob", "mng","qt", ".mpg", ".mpeg", ".3gp", ".mkv"]
+Documents=["txt", "in", "out","pdf","oxps", "epub", "pages", "docx", "doc", "fdf", "ods","odt", "pwi", "xsn", "xps", "dotx", "docm", "dox","rvg", "rtf", "rtfd", "wpd", "xls", "xlsx", "ppt","pptx"]
+ArchiveFiles=["a", "ar", "cpio", "iso", "tar", "gz", "rz", "7z","dmg", "rar", "xar", "zip"] 
+AudioFiles=["aac", "aa", "aac", "dvf", "m4a", "m4b", "m4p", "mp3","msv", "ogg", "oga", "raw", "vox", "wav", "wma"]
+# PLAINTEXT_LIST=[] 
+# PYTHON_LIST=[".py"]
+XML=["xml"]
+Setups=["exe"]
+ShellScripts=["sh"]
+
+
+FILE_FORMATS = {file_format: directory 
+                for directory, file_formats in DIRECTORIES.items() 
+                for file_format in file_formats} 
 
 os.getcwd() #os.getcwd() gets current working directory
-path = input("Enter Path of the folder you want to organise: ")
-#scan directory for files
-# os.scandir(path)
-#all the lists of known extensions
+path = str(input("Enter Path of the folder you want to organise: "))
+files = glob.glob(path + '/*')
 
-HTML_LIST=[".html5", ".html", ".htm", ".xhtml"]
-IMAGES_LIST=[".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",".heif", ".psd"]
-VIDEOS_LIST=[".avi", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob", ".mng",".qt", ".mpg", ".mpeg", ".3gp", ".mkv"]
-DOCUMENTS_LIST=[".txt", ".in", ".out",".pdf",".oxps", ".epub", ".pages", ".docx", ".doc", ".fdf", ".ods",".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt","pptx"]
-ARCHIVES_LIST=[".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z",".dmg", ".rar", ".xar", ".zip"] 
-AUDIO_LIST=[".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3",".msv", "ogg", "oga", ".raw", ".vox", ".wav", ".wma"]
-# PLAINTEXT_LIST=[] 
-PYTHON_LIST=[".py"]
-XML_LIST=[".xml"]
-EXE_LIST=[".exe"]
-SHELL_LIST=[".sh"]
-
-for entry in os.listdir(path):
-	print(entry)
-	for i in DOCUMENTS_LIST:
-		# iterate through the lists to 
-		if(entry.endswith(i)):
-			# scan the folder for directory "Text"
-			dest=path+'\\Text'
-			# if exists, move
-			os.makedirs(dest, exist_ok=True)
-			# else Create folder and move
-			# dest=path+'\\Text'
-			shutil.move(entry, dest)
+def Sorter(**kwargs):
+	for file in files:
+			if Path(file).is_dir():
+				continue
+			# print(file)
+			file_extension = file.split(".")[-1].lower()
+			if file_extension in FILE_FORMATS:
+				to_move_path = Path(FILE_FORMATS[file_extension]) 
+				if kwargs['all']==1:
+					os.makedirs(to_move_path, exist_ok=True)
+					shutil.move(file, to_move_path)
+				if kwargs['Docs']==1:
+					if file_extension in Documents:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['Images']==1:
+					if file_extension in Images:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['Videos']==1:
+					if file_extension in Videos:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['WebPages']==1:
+					if file_extension in WebPages:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['ArchiveFiles']==1:
+					if file_extension in ArchiveFiles:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['AudioFiles']==1:
+					if file_extension in AudioFiles:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['XML']==1:
+					if file_extension in XML:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['Setups']==1:
+					if file_extension in Setups:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
+				if kwargs['ShellScripts']==1:
+					if file_extension in ShellScripts:
+						os.makedirs(to_move_path, exist_ok=True)
+						shutil.move(file, to_move_path)
 		
-	# for i in IMAGES_LIST:
-	# 	# iterate through the lists to 
-	# 	if(entry.endswith(i)):
-	# 		# scan the folder for directory "Text"
-	# 		dest=path+'\\Images'
-	# 		# if exists, move
-	# 		os.makedirs(dest, exist_ok=True)
-	# 		# else Create folder and move
-	# 		shutil.move(entry, dest)
-
-	# for i in VIDEOS_LIST:
-	# 	# iterate through the lists to 
-	# 	if(entry.endswith(i)):
-	# 		# scan the folder for directory "Text"
-	# 		dest=path+'\\Videos'
-	# 		# if exists, move
-	# 		os.makedirs(dest, exist_ok=True)
-	# 		# else Create folder and move
-	# 		# dest=path+'\\Text'
-	# 		shutil.move(entry, dest)
-
-	# for i in ARCHIVES_LIST:
-	# 	# iterate through the lists to 
-	# 	if(entry.endswith(i)):
-	# 		# scan the folder for directory "Text"
-	# 		dest=path+'\\Archives'
-	# 		# if exists, move
-	# 		os.makedirs(dest, exist_ok=True)
-	# 		# else Create folder and move
-	# 		# dest=path+'\\Text'
-	# 		shutil.move(entry, dest)
-
-	# for i in AUDIO_LIST:
-	# 	# iterate through the lists to 
-	# 	if(entry.endswith(i)):
-	# 		# scan the folder for directory "Text"
-	# 		dest=path+'\\Audios'
-	# 		# if exists, move
-	# 		os.makedirs(dest, exist_ok=True)
-	# 		# else Create folder and move
-	# 		# dest=path+'\\Text'
-	# 		shutil.move(entry, dest)
+#Check_Box1
+c1=tk.Checkbutton(window, text="All", variable=i1)
+c1.pack()
+c2=tk.Checkbutton(window, text="Documents", variable=i2)
+c2.pack()
+c3=tk.Checkbutton(window, text="Images", variable=i3)
+c3.pack()
+c4=tk.Checkbutton(window, text="Videos", variable=i4)
+c4.pack()
+c5=tk.Checkbutton(window, text="Archives", variable=i5)
+c5.pack()
+c6=tk.Checkbutton(window, text="Audio", variable=i6)
+c6.pack()
+c7=tk.Checkbutton(window, text="Exe", variable=i7)
+c7.pack()
+c8=tk.Checkbutton(window, text="ShellScripts", variable=i7)
+c8.pack()
+c9=tk.Checkbutton(window, text="WebPages", variable=i7)
+c9.pack()
+c10=tk.Checkbutton(window, text="WebPages", variable=i7)
+c10.pack()
 
 
-# if certain files available move them 
-# create folder if non-existant
+#Button
+b1=tk.Button(window, text="Submit", command=Submit)
+b1.pack()
 
 
-
-
-
+window.mainloop()
